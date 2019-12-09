dd1d687f04b8ee7b7114c48f018bce05e007c776
Usman <usman.aslam0701@gmail.com>
2019-12-09 20:49:33 +0500

    Updates

---
+++ precache-manifest.a58f730882ebd61b074d313ed903429a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "747abacd73d0c8273e8a1098593f5622",
+    "url": "/sportstore/index.html"
+  },
+  {
+    "revision": "34d384c486a806699028",
+    "url": "/sportstore/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "7eb95ec2207f4303b051",
+    "url": "/sportstore/static/css/main.f1ac3de2.chunk.css"
+  },
+  {
+    "revision": "34d384c486a806699028",
+    "url": "/sportstore/static/js/2.4869e7e2.chunk.js"
+  },
+  {
+    "revision": "7eb95ec2207f4303b051",
+    "url": "/sportstore/static/js/main.655a0379.chunk.js"
+  },
+  {
+    "revision": "2209e6828e474b4dfaf3",
+    "url": "/sportstore/static/js/runtime-main.785353b6.js"
+  }
+]);
